472c410e1c1673b21468146441246835062c4cfe
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-05-16 15:13:30 -0500

    assignment 11

--- assignments/assignment11.fib/LCSMain.java
+++ assignments/assignment11.fib/LCSMain.java
@@ -1,12 +1,12 @@
 package miu.edu.algorithms.assignment11.fib;
 
 public class LCSMain {
-    private static int[][] Arr;
+    private static int[][] L;
     private static int count = 0;
 
     public static void main(String[] args) {
-        String s1 = "ccadacbcb";
-        String s2 = "adabc";
+        String s1 = "cababc";
+        String s2 = "abdcb";
         System.out
                 .println("-------------Brute force Version-------------------");
         count = 0;
@@ -33,26 +33,26 @@ public class LCSMain {
     }
 
     public static int memoized(String s1, String s2, int m, int n) {
-        Arr = new int[m + 1][n + 1];
+        L = new int[m + 1][n + 1];
         for (int i = 0; i <= m; i++)
             for (int j = 0; j <= n; j++)
-                Arr[i][j] = -1;
+                L[i][j] = -1;
         return computeMemoized(s1, s2, m, n);
     }
 
     public static int computeMemoized(String s1, String s2, int m, int n) {
-        if (Arr[m][n] < 0) {
+        if (L[m][n] < 0) {
             count++;
             if (n == 0)
-                Arr[m][0] = 0;
+                L[m][0] = 0;
             else if (m == 0)
-                Arr[0][n] = 0;
+                L[0][n] = 0;
             else if (s1.charAt(m - 1) == s2.charAt(n - 1))
-                Arr[m][n] = computeMemoized(s1, s2, m - 1, n - 1) + 1;
+                L[m][n] = computeMemoized(s1, s2, m - 1, n - 1) + 1;
             else
-                Arr[m][n] = Math.max(computeMemoized(s1, s2, m, n - 1),
+                L[m][n] = Math.max(computeMemoized(s1, s2, m, n - 1),
                         computeMemoized(s1, s2, m - 1, n));
         }
-        return Arr[m][n];
+        return L[m][n];
     }
 }
--- src/main/java/miu/edu/algorithms/assignment11/fib/FiboMain.java
+++ src/main/java/miu/edu/algorithms/assignment11/fib/FiboMain.java
@@ -2,7 +2,7 @@ package miu.edu.algorithms.assignment11.fib;
 
 public class FiboMain {
 
-    private static int[] FB;
+    private static int[] F;
     private static int count = 0;
 
     public static void main(String[] args) {
@@ -29,23 +29,23 @@ public class FiboMain {
     }
 
     public static int memoized(int n) {
-        FB = new int[n + 1];
+        F = new int[n + 1];
         for (int i = 0; i <= n; i++) {
-            FB[i] = -1;
+            F[i] = -1;
         }
         return fillFibo(n);
     }
 
     public static int fillFibo(int n) {
         count++;
-        if (FB[n] < 0) {
+        if (F[n] < 0) {
             if (n == 0)
-                FB[n] = 0;
+                F[n] = 0;
             else if (n == 1)
-                FB[n] = 1;
+                F[n] = 1;
             else
-                FB[n] = fillFibo(n - 2) + fillFibo(n - 1);
+                F[n] = fillFibo(n - 2) + fillFibo(n - 1);
         }
-        return FB[n];
+        return F[n];
     }
 }
